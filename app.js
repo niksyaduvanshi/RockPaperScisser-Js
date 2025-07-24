b2c17e5d21ca3b5d1b46faf6e12be4f197b63a0f
niksyaduvanshi <nikhil250804@gmail.com>
2025-07-24 15:20:19 +0530

    final touch

---
+++ app.js
@@ -0,0 +1,41 @@
+let userscore = 0;
+let computerscore = 0;
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector(".msg");
+const userScore = document.querySelector("#user-score");
+const compScore = document.querySelector("#comp-score");
+
+
+const genCompChoice = () => {
+  const option = ["rock", "paper", "scissors"];
+  const index = Math.floor(Math.random() * 3);
+  return option[index];
+};
+
+const playGame = (userChoice) => {
+  console.log("user choice =", userChoice);
+  const compChoice = genCompChoice();
+  console.log("comp Choice =", compChoice);
+  if (userChoice === compChoice) {
+    msg.innerText = "It's a tie!";
+  } else if (
+    (userChoice === "rock" && compChoice === "scissors") ||
+    (userChoice === "paper" && compChoice === "rock") ||
+    (userChoice === "scissors" && compChoice === "paper")
+  ) {
+    userscore++;
+    msg.innerText = "You win!";
+    userScore.innerText = `${userscore}`;
+  } else {
+    computerscore++;
+    msg.innerText =  "Computer wins!";
+    compScore.innerText = `${computerscore}`;
+  }
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    const userChoice = choice.getAttribute("id");
+    playGame(userChoice);
+  });
+});
